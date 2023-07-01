9686f7ef33bafa93debd1cf29cc81009fe9dc59e
yamamo2shun1 <shunichi@made-in-yamamoto.com>
2023-07-01 15:49:03 +0900

    Add HID project files.

---
+++ STM32CubeIDE/C4NDY_hid/Core/Src/main.c
@@ -0,0 +1,375 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "quadspi.h"
+#include "sai.h"
+#include "usb_device.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdbool.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define MATRIX_ROWS 5
+#define MATRIX_COLUMNS 13
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+struct keyboardHID_t {
+	uint8_t modifiers;
+	uint8_t reserved;
+	uint8_t key[6];
+} keyboardHID;
+
+uint16_t keyState[MATRIX_ROWS] = {0x0};
+uint16_t prevKeyState[MATRIX_ROWS] = {0x0};
+
+bool isKeymapIDChanged = false;
+uint8_t keymapID = 0;
+
+// deafult QWERTY layout
+uint8_t keymaps_default[MATRIX_ROWS][MATRIX_COLUMNS] = {
+//       ESC   1     2     3     4     5     6     7     8     9     0     -_    =+
+		{0x29, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x2D, 0x2E},
+//       TAB   q     w     e     r     t     y     u     i     o     p     [{    ]}
+		{0x2B, 0x14, 0x1A, 0x08, 0x15, 0x17, 0x1C, 0x18, 0x0C, 0x12, 0x13, 0x2F, 0x30},
+//       CAPS  a     s     d     f     g     h     j     k     l     ;:    '"    \|
+		{0x39, 0x04, 0x16, 0x07, 0x09, 0x0A, 0x0B, 0x0D, 0x0E, 0x0F, 0x33, 0x34, 0x31},
+//       LSFT  z     x     c     v     b     n     m     ,<    .>    /?    `~    RSFT
+		{0xE1, 0x1D, 0x1B, 0x06, 0x19, 0x05, 0x11, 0x10, 0x36, 0x37, 0x38, 0x35, 0xE5},
+//       LCTRL       GUI   LALT  DEL   BS    ENT   SPC   RALT  ◄     ▼     ▲     ►
+		{0xE0, 0xFF, 0xE3, 0xE2, 0x4C, 0x2A, 0x28, 0x2C, 0xE6, 0x50, 0x51, 0x52, 0x4F}
+};
+
+// Pinky-less Dvorak layout
+uint8_t keymaps_pinkyless[MATRIX_ROWS][MATRIX_COLUMNS] = {
+//       ESC   1     2     3     4     5     6     7     8     9     0     [{    }]
+		{0x29, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x2F, 0x30},
+//       TAB   '"    ,<    o     u     y     f     g     c     r     l     /?    +=
+		{0x2B, 0x34, 0x36, 0x12, 0x18, 0x1C, 0x09, 0x0A, 0x06, 0x15, 0x0F, 0x38, 0x2E},
+//       LCTRL p     i     e     a     .>    d     s     t     h     z     -_    \|
+		{0xE0, 0x13, 0x0C, 0x08, 0x04, 0x37, 0x07, 0x16, 0x17, 0x0B, 0x1D, 0x2D, 0x31},
+//       LSFT  j     q     ;:    k     x     b     m     w     n     v     `~    RSFT
+		{0xE1, 0x0D, 0x14, 0x33, 0x0E, 0x1B, 0x05, 0x10, 0x1A, 0x11, 0x19, 0x35, 0xE5},
+//       CAPS        GUI   LALT  BS    DEL   ENT   SPC   RALT  ◄     ▼     ▲     ►
+		{0x39, 0xFF, 0xE3, 0xE2, 0x2A, 0x4C, 0x28, 0x2C, 0xE6, 0x50, 0x51, 0x52, 0x4F}
+};
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+uint8_t getKeyCode(uint8_t keymapId, uint8_t x, uint8_t y)
+{
+	if (keymapId == 0)
+	{
+		return keymaps_default[x][y];
+	}
+	else
+	{
+		return keymaps_pinkyless[x][y];
+	}
+}
+
+void clearKeys(uint8_t code)
+{
+	if (code == 0xFF)
+	{
+		isKeymapIDChanged = false;
+	}
+	else if (code >= 0xE0 && code <= 0xE7)
+	{
+		keyboardHID.modifiers &= ~(1 << (code - 0xE0));
+	}
+	else
+	{
+		for (int k = 0; k < 6; k++)
+		{
+			if (keyboardHID.key[k] == code)
+			{
+				keyboardHID.key[k] = 0;
+			}
+		}
+	}
+}
+
+void setKeys(uint8_t code)
+{
+	if (code == 0xFF)
+	{
+		if (!isKeymapIDChanged)
+		{
+			if (keymapID == 0)
+			{
+				keymapID = 1;
+				HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
+			}
+			else
+			{
+				keymapID = 0;
+				HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
+			}
+			isKeymapIDChanged = true;
+		}
+	}
+	else if (code >= 0xE0 && code <= 0xE7)
+	{
+		keyboardHID.modifiers |= 1 << (code - 0xE0);
+	}
+	else
+	{
+		for (int k = 0; k < 6; k++)
+		{
+			if (keyboardHID.key[k] == code)
+			{
+				break;
+			}
+			else if (keyboardHID.key[k] == 0x00)
+			{
+				keyboardHID.key[k] = code;
+				break;
+			}
+		}
+	}
+}
+
+void detectSwitches(void)
+{
+	HAL_GPIO_WritePin(HC164_A_GPIO_Port, HC164_A_Pin, GPIO_PIN_RESET);
+
+	for (int i = 0; i < MATRIX_ROWS; i++)
+	{
+		HAL_GPIO_WritePin(HC164_CLK_GPIO_Port, HC164_CLK_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(HC164_CLK_GPIO_Port, HC164_CLK_Pin, GPIO_PIN_RESET);
+
+		HAL_GPIO_WritePin(HC164_A_GPIO_Port, HC164_A_Pin, GPIO_PIN_SET);
+
+		HAL_GPIO_WritePin(HC165_SL_GPIO_Port, HC165_SL_Pin, GPIO_PIN_RESET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(HC165_SL_GPIO_Port, HC165_SL_Pin, GPIO_PIN_SET);
+
+		for (int j = 0; j < 16; j++)
+		{
+			uint8_t jj = 255;
+			if (j < 8)
+			{
+				jj = j + 5;
+			}
+			else if (j >= 11 && j < 16)
+			{
+				jj = j - 11;
+			}
+
+			if (jj < MATRIX_COLUMNS)
+			{
+				if (HAL_GPIO_ReadPin(HC165_QH_GPIO_Port, HC165_QH_Pin))
+				{
+					keyState[i] &= ~((uint16_t)1 << jj);
+
+					if (keyState[i] != prevKeyState[i])
+					{
+						uint8_t keycode = getKeyCode(keymapID, i, (MATRIX_COLUMNS - 1) - jj);
+						clearKeys(keycode);
+					}
+				}
+				else
+				{
+					keyState[i] |= ((uint16_t)1 << jj);
+
+					uint8_t keycode = getKeyCode(keymapID, i, (MATRIX_COLUMNS - 1) - jj);
+					setKeys(keycode);
+				}
+			}
+
+			HAL_GPIO_WritePin(HC165_CLK_GPIO_Port, HC165_CLK_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(HC165_CLK_GPIO_Port, HC165_CLK_Pin, GPIO_PIN_RESET);
+		}
+
+#if 0
+		SEGGER_RTT_printf(0, "key[%d]: 0x%04X\n", i, keyState[i]);
+#endif
+	}
+
+	for (int i = 0; i < MATRIX_ROWS; i++)
+	{
+		if (keyState[i] != 0x0 || (keyState[i] == 0x0 && keyState[i] != prevKeyState[i]))
+		{
+			USBD_HID_SendReport(&hUsbDeviceFS, &keyboardHID, sizeof(keyboardHID));
+			break;
+		}
+	}
+
+	for (int i = 0; i < MATRIX_ROWS; i++)
+	{
+		prevKeyState[i] = keyState[i];
+	}
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SAI1_Init();
+  MX_QUADSPI1_Init();
+  MX_USB_Device_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+	  detectSwitches();
+	  HAL_Delay(1);
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 24;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV6;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
