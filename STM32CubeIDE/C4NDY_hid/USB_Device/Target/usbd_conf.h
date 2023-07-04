6268f479b59bedeffb1bdfaf5938f3f191f9227b
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2023-07-05 07:41:01 +0900

    Configuration DescriptorのbmAttriubtesをバスパワーに変更

--- STM32CubeIDE/C4NDY_hid/USB_Device/Target/usbd_conf.h
+++ STM32CubeIDE/C4NDY_hid/USB_Device/Target/usbd_conf.h
@@ -75,7 +75,7 @@
 /*---------- -----------*/
 #define USBD_LPM_ENABLED     1U
 /*---------- -----------*/
-#define USBD_SELF_POWERED     1U
+#define USBD_SELF_POWERED     0U
 /*---------- -----------*/
 #define HID_FS_BINTERVAL     0xAU
 
