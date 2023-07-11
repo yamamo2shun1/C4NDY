0b5b3adb1f411fdcaf6ff6c2320831032d63f25b
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2023-07-11 09:40:01 +0900

    SigmaStudioで書き出したヘッダーを追加

---
+++ STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Inc/ADAU1761_IC_1_PARAM.h
@@ -0,0 +1,31 @@
+/*
+ * File:           C:\Users\shun\OneDrive - 有限会社山本製作所\YamamotoShunichi\Desktop\adau1761_test\ADAU1761_IC_1_PARAM.h
+ *
+ * Created:        Tuesday, July 11, 2023 8:54:38 AM
+ * Description:    test:IC 1 parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2023 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __ADAU1761_IC_1_PARAM_H__
+#define __ADAU1761_IC_1_PARAM_H__
+
+
+/* Module Modulo Size - Modulo Size*/
+#define MOD_MODULOSIZE_COUNT                           1
+#define MOD_MODULOSIZE_DEVICE                          "IC1"
+#define MOD_MODULOSIZE_MODULO_SIZE_ADDR                0
+#define MOD_MODULOSIZE_MODULO_SIZE_FIXPT               0x00001000
+#define MOD_MODULOSIZE_MODULO_SIZE_VALUE               SIGMASTUDIOTYPE_INTEGER_CONVERT(4096)
+#define MOD_MODULOSIZE_MODULO_SIZE_TYPE                SIGMASTUDIOTYPE_INTEGER
+
+#endif
