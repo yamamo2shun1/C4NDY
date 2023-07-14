9ad075e51acc526df031ea6cd53846a725c14f9d
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2023-07-14 23:25:28 +0900

    2重で初期化してたので1個消す

--- STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Src/main.c
+++ STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Src/main.c
@@ -81,7 +81,7 @@ uint8_t keymaps_default[MATRIX_ROWS][MATRIX_COLUMNS] = {
 		{0x39, 0x04, 0x16, 0x07, 0x09, 0x0A, 0x0B, 0x0D, 0x0E, 0x0F, 0x33, 0x34, 0x31},
 //       LSFT  z     x     c     v     b     n     m     ,<    .>    /?    RSFT  `~
 		{0xE1, 0x1D, 0x1B, 0x06, 0x19, 0x05, 0x11, 0x10, 0x36, 0x37, 0x38, 0xE5, 0x35},
-//       GUI               LALT  BS    ENT   SPC   Int4  RCTRL ????¿½?¿½??¿½?¿½???¿½?¿½??¿½?¿½?     â–¼     â–²     â–º
+//       GUI               LALT  BS    ENT   SPC   Int4  RCTRL ????ï¿½ï¿½?ï¿½ï¿½??ï¿½ï¿½?ï¿½ï¿½???ï¿½ï¿½?ï¿½ï¿½??ï¿½ï¿½?ï¿½ï¿½?     â–¼     â–²     â–º
 		{0xE3, 0xFE, 0xFF, 0xE2, 0x2A, 0x28, 0x2C, 0x8A, 0xE4, 0x50, 0x51, 0x52, 0x4F}
 };
 
@@ -95,7 +95,7 @@ uint8_t keymaps_pinkyless[MATRIX_ROWS][MATRIX_COLUMNS] = {
 		{0xE0, 0x13, 0x0C, 0x08, 0x04, 0x37, 0x07, 0x16, 0x17, 0x0B, 0x1D, 0x2D, 0x31},
 //       LSFT  j     q     ;:    k     x     b     m     w     n     v     RSFT  `~
 		{0xE1, 0x0D, 0x14, 0x33, 0x0E, 0x1B, 0x05, 0x10, 0x1A, 0x11, 0x19, 0xE5, 0x35},
-//       GUI               LALT  BS    DEL   ENT   SPC   CAPS  ????¿½?¿½??¿½?¿½???¿½?¿½??¿½?¿½?     â–¼     â–²     â–º
+//       GUI               LALT  BS    DEL   ENT   SPC   CAPS  ????ï¿½ï¿½?ï¿½ï¿½??ï¿½ï¿½?ï¿½ï¿½???ï¿½ï¿½?ï¿½ï¿½??ï¿½ï¿½?ï¿½ï¿½?     â–¼     â–²     â–º
 		{0xE3, 0xFE, 0xFF, 0xE2, 0x2A, 0x4C, 0x28, 0x2C, 0x39, 0x50, 0x51, 0x52, 0x4F}
 };
 /* USER CODE END PV */
@@ -297,7 +297,6 @@ int main(void)
   MX_QUADSPI1_Init();
   MX_I2C2_Init();
   MX_ADC1_Init();
-  MX_USB_PCD_Init();
   /* USER CODE BEGIN 2 */
   //default_download_IC_1();
 
