77e18a212b66503d6a1c920958671a28be605f23
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2023-07-25 15:17:25 +0900

    文字化けを修正

--- STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Src/main.c
+++ STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Src/main.c
@@ -125,7 +125,7 @@ uint8_t keymaps_default[MATRIX_ROWS][MATRIX_COLUMNS] = {
 		{0x39, 0x04, 0x16, 0x07, 0x09, 0x0A, 0x0B, 0x0D, 0x0E, 0x0F, 0x33, 0x34, 0x31},
 //       LSFT  z     x     c     v     b     n     m     ,<    .>    /?    RSFT  `~
 		{0xE1, 0x1D, 0x1B, 0x06, 0x19, 0x05, 0x11, 0x10, 0x36, 0x37, 0x38, 0xE5, 0x35},
-//       GUI               LALT  BS    ENT   SPC   Int4  RCTRL �?     ▼     ▲     ►
+//       GUI               LALT  BS    ENT   SPC   Int4  RCTRL ◀     ▼     ▲     ►
 		{0xE3, 0xFE, 0xFF, 0xE2, 0x2A, 0x28, 0x2C, 0x8A, 0xE4, 0x50, 0x51, 0x52, 0x4F}
 };
 
@@ -139,7 +139,7 @@ uint8_t keymaps_pinkyless[MATRIX_ROWS][MATRIX_COLUMNS] = {
 		{0xE0, 0x13, 0x0C, 0x08, 0x04, 0x37, 0x07, 0x16, 0x17, 0x0B, 0x1D, 0x2D, 0x31},
 //       LSFT  j     q     ;:    k     x     b     m     w     n     v     RSFT  `~
 		{0xE1, 0x0D, 0x14, 0x33, 0x0E, 0x1B, 0x05, 0x10, 0x1A, 0x11, 0x19, 0xE5, 0x35},
-//       GUI               LALT  BS    DEL   ENT   SPC   CAPS  �?     ▼     ▲     ►
+//       GUI               LALT  BS    DEL   ENT   SPC   CAPS  ◀     ▼     ▲     ►
 		{0xE3, 0xFE, 0xFF, 0xE2, 0x2A, 0x4C, 0x28, 0x2C, 0x39, 0x50, 0x51, 0x52, 0x4F}
 };
 /* USER CODE END PV */
@@ -727,17 +727,6 @@ int main(void)
   while (1)
   {
 	tud_task();
-
-	//detectSwitches();
-
-#if 0
-	if (!HAL_GPIO_ReadPin(USER_SW_GPIO_Port, USER_SW_Pin))
-	{
-		SEGGER_RTT_printf(0, "press!\n");
-		resetKeys();
-		USBD_HID_SendReport(&hUsbDeviceFS, &keyboardHID, sizeof(keyboardHID));
-	}
-#endif
 
 	//SEGGER_RTT_printf(0, "pot = %d, %d\n", pot_value[0], pot_value[1]);
 
