843cf7f126550c8d4ed5a3cf272e4c8855884f95
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2023-08-01 15:50:44 +0900

    SAIをDMA化

--- STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Inc/stm32g4xx_it.h
+++ STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Inc/stm32g4xx_it.h
@@ -55,6 +55,7 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void DMA1_Channel1_IRQHandler(void);
 void DMA1_Channel2_IRQHandler(void);
 void ADC1_2_IRQHandler(void);
 void USB_HP_IRQHandler(void);
--- STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Src/stm32g4xx_it.c
+++ STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Src/stm32g4xx_it.c
@@ -57,6 +57,7 @@
 /* External variables --------------------------------------------------------*/
 extern DMA_HandleTypeDef hdma_adc1;
 extern ADC_HandleTypeDef hadc1;
+extern DMA_HandleTypeDef hdma_sai1_b;
 extern SAI_HandleTypeDef hsai_BlockB1;
 extern TIM_HandleTypeDef htim6;
 extern PCD_HandleTypeDef hpcd_USB_FS;
@@ -201,6 +202,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32g4xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles DMA1 channel1 global interrupt.
+  */
+void DMA1_Channel1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_sai1_b);
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
 
 /**
   * @brief This function handles DMA1 channel2 global interrupt.
