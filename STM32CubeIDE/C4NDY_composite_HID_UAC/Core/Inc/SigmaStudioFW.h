4d4c667c25a49a9fd3638fe56be06f7fd70c57dc
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2023-08-01 15:52:00 +0900

    ADAU1761のパラメータ変更用の関数を実装

--- STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Inc/SigmaStudioFW.h
+++ STM32CubeIDE/C4NDY_composite_HID_UAC/Core/Inc/SigmaStudioFW.h
@@ -48,6 +48,12 @@ void SIGMA_WRITE_REGISTER_BLOCK(uint16_t devAddress, uint16_t address, uint32_t 
 {
 	HAL_I2C_Mem_Write(&hi2c2, devAddress, address, I2C_MEMADD_SIZE_16BIT, pData, length, 10000);
 }
+
+void SIGMA_SAFELOAD_WRITE_DATA(uint8_t devAddress, uint16_t dataAddress, uint16_t length, uint8_t *pData)
+{
+	HAL_I2C_Mem_Write(&hi2c2, devAddress, dataAddress, I2C_MEMADD_SIZE_16BIT, pData, length, 10000);
+}
+
 /* 
  * TODO: CUSTOM MACRO IMPLEMENTATION
  * Writes delay (in ms) 
