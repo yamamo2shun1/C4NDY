f10595f63133a1a6d09ab431c9c518a10b82ad7f
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2023-09-14 16:18:45 +0900

    ADAU1761へのデータ書き込み時にエラーが出たらprintする

--- STM32CubeIDE/C4NDY_KeyVLM/Core/Inc/SigmaStudioFW.h
+++ STM32CubeIDE/C4NDY_KeyVLM/Core/Inc/SigmaStudioFW.h
@@ -48,7 +48,12 @@ typedef unsigned char  ADI_REG_TYPE;
 //#define SIGMA_WRITE_REGISTER_BLOCK( devAddress, address, length, pData ) {/*TODO: implement macro or define as function*/}
 void SIGMA_WRITE_REGISTER_BLOCK(uint16_t devAddress, uint16_t address, uint32_t length, uint8_t *pData)
 {
-	HAL_I2C_Mem_Write(&hi2c2, devAddress, address, I2C_MEMADD_SIZE_16BIT, pData, length, 10000);
+	HAL_StatusTypeDef status;
+	status = HAL_I2C_Mem_Write(&hi2c2, devAddress, address, I2C_MEMADD_SIZE_16BIT, pData, length, 10000);
+	if (status != HAL_OK)
+	{
+		SEGGER_RTT_printf(0, "[%X] i2c write error\n", address);
+	}
 }
 
 void SIGMA_SAFELOAD_WRITE_DATA(uint8_t devAddress, uint16_t dataAddress, uint16_t length, uint8_t *pData)
