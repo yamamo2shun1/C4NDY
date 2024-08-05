e59edebc03cf776e685f6515eaff5a9667a3f02a
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2024-08-05 15:56:27 +0900

    Refactored.

--- STM32CubeIDE/C4NDY_KeyVLM/Core/Inc/keyboard.h
+++ STM32CubeIDE/C4NDY_KeyVLM/Core/Inc/keyboard.h
@@ -8,110 +8,111 @@
 #ifndef INC_KEYBOARD_H_
 #define INC_KEYBOARD_H_
 
-#define MATRIX_ROWS 5
+#define MATRIX_ROWS    5
 #define MATRIX_COLUMNS 13
 
-#define SC_A        0x04 // a
-#define SC_B        0x05 // b
-#define SC_C        0x06 // c
-#define SC_D        0x07 // d
-#define SC_E        0x08 // e
-#define SC_F        0x09 // f
-#define SC_G        0x0A // g
-#define SC_H        0x0B // h
-#define SC_I        0x0C // i
-#define SC_J        0x0D // j
-#define SC_K        0x0E // k
-#define SC_L        0x0F // l
-#define SC_M        0x10 // m
-#define SC_N        0x11 // n
-#define SC_O        0x12 // o
-#define SC_P        0x13 // p
-#define SC_Q        0x14 // q
-#define SC_R        0x15 // r
-#define SC_S        0x16 // s
-#define SC_T        0x17 // t
-#define SC_U        0x18 // u
-#define SC_V        0x19 // v
-#define SC_W        0x1A // w
-#define SC_X        0x1B // x
-#define SC_Y        0x1C // y
-#define SC_Z        0x1D // z
-#define SC_1        0x1E // 1!
-#define SC_2        0x1F // 2@
-#define SC_3        0x20 // 3#
-#define SC_4        0x21 // 4$
-#define SC_5        0x22 // 5%
-#define SC_6        0x23 // 6^
-#define SC_7        0x24 // 7&
-#define SC_8        0x25 // 8*
-#define SC_9        0x26 // 9(
-#define SC_0        0x27 // 0)
-#define SC_ENTER    0x28 // Enter
-#define SC_ESC      0x29 // Escape
-#define SC_BS       0x2A // Backspace
-#define SC_TAB      0x2B // Tab
-#define SC_SPACE    0x2C // Space
-#define SC_MINUS    0x2D // -_
-#define SC_EQUAL    0x2E // =+
-#define SC_LSB      0x2F // [{
-#define SC_RSB      0x30 // ]}
-#define SC_BSLASH   0x31 // \|
-#define SC_SC       0x33 // ;:
-#define SC_APS      0x34 // '"
-#define SC_GA       0x35 // `~
-#define SC_COMMA    0x36 // ,<
-#define SC_PERIOD   0x37 // .>
-#define SC_SLASH    0x38 // /?
-#define SC_CAPSLOCK 0x39 // Caps Lock
-#define SC_F1       0x3A // F1
-#define SC_F2       0x3B // F2
-#define SC_F3       0x3C // F3
-#define SC_F4       0x3D // F4
-#define SC_F5       0x3E // F5
-#define SC_F6       0x3F // F6
-#define SC_F7       0x40 // F7
-#define SC_F8       0x41 // F8
-#define SC_F9       0x42 // F9
-#define SC_F10      0x43 // F10
-#define SC_F11      0x44 // F11
-#define SC_F12      0x45 // F12
-#define SC_PRNTSCRN 0x46 // Print Screen
-#define SC_SCRLLOCK 0x47 // Scroll Lock
-#define SC_PAUSE    0x48 // Pause
-#define SC_INSERT   0x49 // Insert
-#define SC_HOME     0x4A // Home
-#define SC_PAGEUP   0x4B // Page Up
-#define SC_DELETE   0x4C // Delete
-#define SC_END      0x4D // End
-#define SC_PAGEDOWN 0x4E // Page Down
-#define SC_RIGHT    0x4F // Right
-#define SC_LEFT     0x50 // Left
-#define SC_DOWN     0x51 // Down
-#define SC_UP       0x52 // Up
-#define SC_NUMLOCK  0x53 // Num Lock
-#define SC_KATAKANA 0x88 // カタカナ ひらがな
-#define SC_YEN      0x89 // ￥|
-#define SC_HENKAN   0x8A // 変換
-#define SC_MUHENKAN 0x8B // 無変換
-#define SC_LCONTROL 0xE0 // Left Control
-#define SC_LSHIFT   0xE1 // Left Shift
-#define SC_LALT     0xE2 // Left Alt
-#define SC_LGUI     0xE3 // Left GUI(Win/Command)
-#define SC_RCONTROL 0xE4 // Right Control
-#define SC_RSHIFT   0xE5 // Right Shift
-#define SC_RALT     0xE6 // Right Alt
-#define SC_RGUI     0xE7 // Right GUI(Win/Command)
+#define SC_A        0x04  // a
+#define SC_B        0x05  // b
+#define SC_C        0x06  // c
+#define SC_D        0x07  // d
+#define SC_E        0x08  // e
+#define SC_F        0x09  // f
+#define SC_G        0x0A  // g
+#define SC_H        0x0B  // h
+#define SC_I        0x0C  // i
+#define SC_J        0x0D  // j
+#define SC_K        0x0E  // k
+#define SC_L        0x0F  // l
+#define SC_M        0x10  // m
+#define SC_N        0x11  // n
+#define SC_O        0x12  // o
+#define SC_P        0x13  // p
+#define SC_Q        0x14  // q
+#define SC_R        0x15  // r
+#define SC_S        0x16  // s
+#define SC_T        0x17  // t
+#define SC_U        0x18  // u
+#define SC_V        0x19  // v
+#define SC_W        0x1A  // w
+#define SC_X        0x1B  // x
+#define SC_Y        0x1C  // y
+#define SC_Z        0x1D  // z
+#define SC_1        0x1E  // 1!
+#define SC_2        0x1F  // 2@
+#define SC_3        0x20  // 3#
+#define SC_4        0x21  // 4$
+#define SC_5        0x22  // 5%
+#define SC_6        0x23  // 6^
+#define SC_7        0x24  // 7&
+#define SC_8        0x25  // 8*
+#define SC_9        0x26  // 9(
+#define SC_0        0x27  // 0)
+#define SC_ENTER    0x28  // Enter
+#define SC_ESC      0x29  // Escape
+#define SC_BS       0x2A  // Backspace
+#define SC_TAB      0x2B  // Tab
+#define SC_SPACE    0x2C  // Space
+#define SC_MINUS    0x2D  // -_
+#define SC_EQUAL    0x2E  // =+
+#define SC_LSB      0x2F  // [{
+#define SC_RSB      0x30  // ]}
+#define SC_BSLASH   0x31  // \|
+#define SC_SC       0x33  // ;:
+#define SC_APS      0x34  // '"
+#define SC_GA       0x35  // `~
+#define SC_COMMA    0x36  // ,<
+#define SC_PERIOD   0x37  // .>
+#define SC_SLASH    0x38  // /?
+#define SC_CAPSLOCK 0x39  // Caps Lock
+#define SC_F1       0x3A  // F1
+#define SC_F2       0x3B  // F2
+#define SC_F3       0x3C  // F3
+#define SC_F4       0x3D  // F4
+#define SC_F5       0x3E  // F5
+#define SC_F6       0x3F  // F6
+#define SC_F7       0x40  // F7
+#define SC_F8       0x41  // F8
+#define SC_F9       0x42  // F9
+#define SC_F10      0x43  // F10
+#define SC_F11      0x44  // F11
+#define SC_F12      0x45  // F12
+#define SC_PRNTSCRN 0x46  // Print Screen
+#define SC_SCRLLOCK 0x47  // Scroll Lock
+#define SC_PAUSE    0x48  // Pause
+#define SC_INSERT   0x49  // Insert
+#define SC_HOME     0x4A  // Home
+#define SC_PAGEUP   0x4B  // Page Up
+#define SC_DELETE   0x4C  // Delete
+#define SC_END      0x4D  // End
+#define SC_PAGEDOWN 0x4E  // Page Down
+#define SC_RIGHT    0x4F  // Right
+#define SC_LEFT     0x50  // Left
+#define SC_DOWN     0x51  // Down
+#define SC_UP       0x52  // Up
+#define SC_NUMLOCK  0x53  // Num Lock
+#define SC_KATAKANA 0x88  // カタカナ ひらがな
+#define SC_YEN      0x89  // ￥|
+#define SC_HENKAN   0x8A  // 変換
+#define SC_MUHENKAN 0x8B  // 無変換
+#define SC_LCONTROL 0xE0  // Left Control
+#define SC_LSHIFT   0xE1  // Left Shift
+#define SC_LALT     0xE2  // Left Alt
+#define SC_LGUI     0xE3  // Left GUI(Win/Command)
+#define SC_RCONTROL 0xE4  // Right Control
+#define SC_RSHIFT   0xE5  // Right Shift
+#define SC_RALT     0xE6  // Right Alt
+#define SC_RGUI     0xE7  // Right GUI(Win/Command)
 
 // original code
-#define SC_LNPH     0xFE // Line Phono Swtich
-#define SC_LAYOUT   0xFF // Layout Switch
+#define SC_MGAIN_UP   0xFA  // Master Gain Up
+#define SC_MGAIN_DOWN 0xFB  // Master Gain Down
+#define SC_UPPER      0xFC  // Upper
+#define SC_LNPH       0xFD  // Line Phono Swtich
+#define SC_LAYOUT     0xFE  // Layout Switch
+#define SC_NULL       0xFF
 
-// deafult QWERTY layout
-extern uint8_t keymaps_default[MATRIX_ROWS][MATRIX_COLUMNS];
-
-// Pinky-less Dvorak layout
-extern uint8_t keymaps_pinkyless[MATRIX_ROWS][MATRIX_COLUMNS];
+#define H 0
+#define V 1
 
 void setLinePhonoSW(uint8_t val);
 uint8_t getLinePhonoSW(void);
--- STM32CubeIDE/C4NDY_KeyVLM/Core/Src/keyboard.c
+++ STM32CubeIDE/C4NDY_KeyVLM/Core/Src/keyboard.c
@@ -9,40 +9,42 @@
 #include "keyboard.h"
 #include "audio_control.h"
 
-struct keyboardHID_t {
-	uint8_t modifiers;
-	uint8_t reserved;
-	uint8_t key[6];
+struct keyboardHID_t
+{
+    uint8_t modifiers;
+    uint8_t reserved;
+    uint8_t key[6];
 } keyboardHID;
 
-uint16_t keyState[MATRIX_ROWS] = {0x0};
+uint16_t keyState[MATRIX_ROWS]     = {0x0};
 uint16_t prevKeyState[MATRIX_ROWS] = {0x0};
 
-bool isSwitchDFUMode = false;
+bool isSwitchDFUMode      = false;
 uint32_t longPressCounter = 0;
 
 bool isKeymapIDChanged = false;
-uint8_t keymapID = 0;
+uint8_t keymapID       = 0;
 
 bool isLinePhonoSWChanged = false;
-uint8_t linePhonoSW = 0;
+uint8_t linePhonoSW       = 0;
 
-// deafult QWERTY layout
-uint8_t keymaps_default[MATRIX_ROWS][MATRIX_COLUMNS] = {
-	{SC_ESC,      SC_1,    SC_2,      SC_3,    SC_4,  SC_5,     SC_6,     SC_7,      SC_8,        SC_9,      SC_0,     SC_MINUS,  SC_EQUAL},
-	{SC_TAB,      SC_Q,    SC_W,      SC_E,    SC_R,  SC_T,     SC_Y,     SC_U,      SC_I,        SC_O,      SC_P,     SC_LSB,    SC_RSB},
-	{SC_CAPSLOCK, SC_A,    SC_S,      SC_D,    SC_F,  SC_G,     SC_H,     SC_J,      SC_K,        SC_L,      SC_SC,    SC_APS,    SC_YEN},
-	{SC_LSHIFT,   SC_Z,    SC_X,      SC_C,    SC_V,  SC_B,     SC_N,     SC_M,      SC_COMMA,    SC_PERIOD, SC_SLASH, SC_RSHIFT, SC_GA},
-	{SC_LGUI,     SC_LNPH, SC_LAYOUT, SC_LALT, SC_BS, SC_ENTER, SC_SPACE, SC_HENKAN, SC_RCONTROL, SC_LEFT,   SC_DOWN,  SC_UP,     SC_RIGHT}
-};
-
-// Improved Dvorak layout
-uint8_t keymaps_layout2[MATRIX_ROWS][MATRIX_COLUMNS] = {
-	{SC_ESC,      SC_1,    SC_2,      SC_3,    SC_4,  SC_5,      SC_6,     SC_7,     SC_8,        SC_9,    SC_0,    SC_LSB,    SC_RSB},
-	{SC_TAB,      SC_APS,  SC_COMMA,  SC_O,    SC_U,  SC_Y,      SC_F,     SC_G,     SC_C,        SC_R,    SC_L,    SC_SLASH,  SC_EQUAL},
-	{SC_LCONTROL, SC_P,    SC_I,      SC_E,    SC_A,  SC_PERIOD, SC_D,     SC_S,     SC_T,        SC_H,    SC_Z,    SC_MINUS,  SC_BSLASH},
-	{SC_LSHIFT,   SC_J,    SC_Q,      SC_SC,   SC_K,  SC_X,      SC_B,     SC_M,     SC_W,        SC_N,    SC_V,    SC_RSHIFT, SC_GA},
-	{SC_LGUI,     SC_LNPH, SC_LAYOUT, SC_LALT, SC_BS, SC_DELETE, SC_ENTER, SC_SPACE, SC_CAPSLOCK, SC_LEFT, SC_DOWN, SC_UP,     SC_RIGHT}
+uint8_t keymaps_normal[2][MATRIX_ROWS][MATRIX_COLUMNS] = {
+    // clang-format off
+    {
+        {SC_ESC,      SC_1,    SC_2,      SC_3,    SC_4,  SC_5,     SC_6,     SC_7,      SC_8,        SC_9,      SC_0,     SC_MINUS,  SC_EQUAL},
+        {SC_TAB,      SC_Q,    SC_W,      SC_E,    SC_R,  SC_T,     SC_Y,     SC_U,      SC_I,        SC_O,      SC_P,     SC_LSB,    SC_RSB  },
+        {SC_CAPSLOCK, SC_A,    SC_S,      SC_D,    SC_F,  SC_G,     SC_H,     SC_J,      SC_K,        SC_L,      SC_SC,    SC_APS,    SC_YEN  },
+        {SC_LSHIFT,   SC_Z,    SC_X,      SC_C,    SC_V,  SC_B,     SC_N,     SC_M,      SC_COMMA,    SC_PERIOD, SC_SLASH, SC_RSHIFT, SC_GA   },
+        {SC_LGUI,     SC_LNPH, SC_LAYOUT, SC_LALT, SC_BS, SC_ENTER, SC_SPACE, SC_HENKAN, SC_RCONTROL, SC_LEFT,   SC_DOWN,  SC_UP,     SC_RIGHT}
+    },
+    {
+        {SC_ESC,      SC_1,    SC_2,      SC_3,    SC_4,  SC_5,      SC_6,     SC_7,     SC_8,        SC_9,    SC_0,    SC_LSB,    SC_RSB   },
+        {SC_TAB,      SC_APS,  SC_COMMA,  SC_O,    SC_U,  SC_Y,      SC_F,     SC_G,     SC_C,        SC_R,    SC_L,    SC_SLASH,  SC_EQUAL },
+        {SC_LCONTROL, SC_P,    SC_I,      SC_E,    SC_A,  SC_PERIOD, SC_D,     SC_S,     SC_T,        SC_H,    SC_Z,    SC_MINUS,  SC_BSLASH},
+        {SC_LSHIFT,   SC_J,    SC_Q,      SC_SC,   SC_K,  SC_X,      SC_B,     SC_M,     SC_W,        SC_N,    SC_V,    SC_RSHIFT, SC_GA    },
+        {SC_LGUI,     SC_LNPH, SC_LAYOUT, SC_LALT, SC_BS, SC_DELETE, SC_ENTER, SC_SPACE, SC_CAPSLOCK, SC_LEFT, SC_DOWN, SC_UP,     SC_RIGHT }
+    }
+    // clang-format on
 };
 
 // Invoked when sent REPORT successfully to host
@@ -65,22 +67,22 @@ void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_
 
 void setLinePhonoSW(uint8_t val)
 {
-	linePhonoSW = val;
-	if (linePhonoSW == 0)
-	{
-		send_switch_to_linein();
-		HAL_GPIO_WritePin(LP_LED_GPIO_Port, LP_LED_Pin, GPIO_PIN_RESET);
-	}
-	else
-	{
-		send_switch_to_phonoin();
-		HAL_GPIO_WritePin(LP_LED_GPIO_Port, LP_LED_Pin, GPIO_PIN_SET);
-	}
+    linePhonoSW = val;
+    if (linePhonoSW == 0)
+    {
+        send_switch_to_linein();
+        HAL_GPIO_WritePin(LP_LED_GPIO_Port, LP_LED_Pin, GPIO_PIN_RESET);
+    }
+    else
+    {
+        send_switch_to_phonoin();
+        HAL_GPIO_WritePin(LP_LED_GPIO_Port, LP_LED_Pin, GPIO_PIN_SET);
+    }
 }
 
 uint8_t getLinePhonoSW(void)
 {
-	return linePhonoSW;
+    return linePhonoSW;
 }
 
 // Invoked when received GET_REPORT control request
@@ -88,48 +90,38 @@ uint8_t getLinePhonoSW(void)
 // Return zero will cause the stack to STALL request
 uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
 {
-  // TODO not Implemented
-  (void) instance;
-  (void) report_id;
-  (void) report_type;
-  (void) buffer;
-  (void) reqlen;
+    // TODO not Implemented
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
 
-  return 0;
+    return 0;
 }
 
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
 {
-  (void) instance;
+    (void) instance;
 
-  SEGGER_RTT_printf(0, "report_id = %d\n", report_id);
-  SEGGER_RTT_printf(0, "report_type = %d\n", report_type);
-  for (int i = 0; i < bufsize; i++)
-  {
-	  SEGGER_RTT_printf(0, "buf[%d] = %d\n", i, buffer[i]);
-  }
-  SEGGER_RTT_printf(0, "bufsize = %d\n", bufsize);
+    SEGGER_RTT_printf(0, "report_id = %d\n", report_id);
+    SEGGER_RTT_printf(0, "report_type = %d\n", report_type);
+    for (int i = 0; i < bufsize; i++)
+    {
+        SEGGER_RTT_printf(0, "buf[%d] = %d\n", i, buffer[i]);
+    }
+    SEGGER_RTT_printf(0, "bufsize = %d\n", bufsize);
 
-  uint8_t rbuf[16] = {0x00};
-  if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x00)
-  {
-	  SEGGER_RTT_printf(0, "read:\n");
-	  if (keymapID == 0)
-	  {
-		  for (int j = 0; j < 13; j++)
-		  {
-			  rbuf[j] = keymaps_default[buffer[0] - 0xF0][j];
-		  }
-	  }
-	  else
-	  {
-		  for (int j = 0; j < 13; j++)
-		  {
-			  rbuf[j] = keymaps_layout2[buffer[0] - 0xF0][j];
-		  }
-	  }
+    uint8_t rbuf[16] = {0x00};
+    if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x00)
+    {
+        SEGGER_RTT_printf(0, "read:\n");
+        for (int j = 0; j < MATRIX_COLUMNS; j++)
+        {
+            rbuf[j] = keymaps_normal[keymapID][buffer[0] - 0xF0][j];
+        }
 
 #if 0
 	  for (int i = 0; i < 16; i++)
@@ -137,73 +129,72 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
 		  SEGGER_RTT_printf(0, "rbuf[%d] = %d\n", i, rbuf[i]);
 	  }
 #endif
-	  tud_hid_n_report(1, 0, rbuf, 16);
+        tud_hid_n_report(1, 0, rbuf, 16);
+    }
+    else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x01)
+    {
+        SEGGER_RTT_printf(0, "write to layout1:\n");
 
-  }
-  else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x01)
-  {
-	  SEGGER_RTT_printf(0, "write to layout1:\n");
+        for (int j = 0; j < MATRIX_COLUMNS; j++)
+        {
+            keymaps_normal[0][buffer[0] - 0xF0][j] = buffer[j + 2];
+        }
+    }
+    else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x04)
+    {
+        SEGGER_RTT_printf(0, "write to layout2:\n");
 
-	  for (int j = 0; j < 13; j++)
-	  {
-		  keymaps_default[buffer[0] - 0xF0][j] = buffer[j + 2];
-	  }
-  }
-  else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x02)
-  {
-	  SEGGER_RTT_printf(0, "write to layout2:\n");
+        for (int j = 0; j < MATRIX_COLUMNS; j++)
+        {
+            keymaps_normal[1][buffer[0] - 0xF0][j] = buffer[j + 2];
+        }
+    }
+    else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x07)
+    {
+        SEGGER_RTT_printf(0, "read from layout1:\n");
+        for (int j = 0; j < MATRIX_COLUMNS; j++)
+        {
+            rbuf[j] = keymaps_normal[0][buffer[0] - 0xF0][j];
+        }
 
-	  for (int j = 0; j < 13; j++)
-	  {
-		  keymaps_layout2[buffer[0] - 0xF0][j] = buffer[j + 2];
-	  }
-  }
-  else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x03)
-  {
-	  SEGGER_RTT_printf(0, "read from layout1:\n");
-	  for (int j = 0; j < 13; j++)
-	  {
-		  rbuf[j] = keymaps_default[buffer[0] - 0xF0][j];
-	  }
+        tud_hid_n_report(1, 0, rbuf, 16);
+    }
+    else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x0A)
+    {
+        SEGGER_RTT_printf(0, "read from layout2:\n");
+        for (int j = 0; j < MATRIX_COLUMNS; j++)
+        {
+            rbuf[j] = keymaps_normal[1][buffer[0] - 0xF0][j];
+        }
 
-	  tud_hid_n_report(1, 0, rbuf, 16);
-  }
-  else if (buffer[0] >= 0xF0 && buffer[0] <= 0xF4 && buffer[1] == 0x04)
-  {
-	  SEGGER_RTT_printf(0, "read from layout2:\n");
-	  for (int j = 0; j < 13; j++)
-	  {
-		  rbuf[j] = keymaps_layout2[buffer[0] - 0xF0][j];
-	  }
+        tud_hid_n_report(1, 0, rbuf, 16);
+    }
+    else if (buffer[0] == 0xF5)
+    {
+        SEGGER_RTT_printf(0, "erase & write FLASH...\n");
+        HAL_FLASH_Unlock();
 
-	  tud_hid_n_report(1, 0, rbuf, 16);
-  }
-  else if (buffer[0] == 0xF5)
-  {
-	  SEGGER_RTT_printf(0, "erase & write FLASH...\n");
-	  HAL_FLASH_Unlock();
+        erase_flash_data();
 
-	  erase_flash_data();
+        write_flash_data(0, linePhonoSW);
+        write_flash_data(1, 99);
 
-	  write_flash_data(0, linePhonoSW);
-	  write_flash_data(1, 99);
+        for (int i = 0; i < MATRIX_ROWS; i++)
+        {
+            for (int j = 0; j < MATRIX_COLUMNS; j++)
+            {
+                write_flash_data(2 + 0 * (MATRIX_ROWS * MATRIX_COLUMNS) + i * MATRIX_COLUMNS + j, getKeyCode(0, i, j));
+                write_flash_data(2 + 1 * (MATRIX_ROWS * MATRIX_COLUMNS) + i * MATRIX_COLUMNS + j, getKeyCode(1, i, j));
+            }
+        }
 
-	  for (int i = 0; i < 5; i++)
-	  {
-		  for (int j = 0; j < 13; j++)
-		  {
-			  write_flash_data(2 + 0 * 65 + i * 13 + j, keymaps_default[i][j]);
-			  write_flash_data(2 + 1 * 65 + i * 13 + j, keymaps_layout2[i][j]);
-		  }
-	  }
+        HAL_FLASH_Lock();
 
-	  HAL_FLASH_Lock();
-
-	  uint8_t rbuf[16] = {0x00};
-	  rbuf[1] = 0xF5;
-	  rbuf[2] = 0x01;
-	  tud_hid_n_report(1, 0, rbuf, 16);
-  }
+        uint8_t rbuf[16] = {0x00};
+        rbuf[1]          = 0xF5;
+        rbuf[2]          = 0x01;
+        tud_hid_n_report(1, 0, rbuf, 16);
+    }
 
 #if 0
   if (report_type == HID_REPORT_TYPE_OUTPUT)
@@ -234,208 +225,194 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
 
 uint8_t getKeyCode(uint8_t keymapId, uint8_t x, uint8_t y)
 {
-	if (keymapId == 0)
-	{
-		return keymaps_default[x][y];
-	}
-	else
-	{
-		return keymaps_layout2[x][y];
-	}
+    return keymaps_normal[keymapId][x][y];
 }
 
 void setKeyCode(uint8_t keymapId, uint8_t x, uint8_t y, uint8_t code)
 {
-	if (keymapId == 0)
-	{
-		keymaps_default[x][y] = code;
-	}
-	else
-	{
-		keymaps_layout2[x][y] = code;
-	}
+    keymaps_normal[keymapId][x][y] = code;
 }
 
 void resetKeys(void)
 {
-	keyboardHID.modifiers = 0;
-	for (int k = 0; k < 6; k++)
-	{
-		keyboardHID.key[k] = 0;
-	}
+    keyboardHID.modifiers = 0;
+    for (int k = 0; k < 6; k++)
+    {
+        keyboardHID.key[k] = 0;
+    }
 }
 
 void clearKeys(uint8_t code)
 {
-	if (code == SC_LAYOUT)
-	{
-		isKeymapIDChanged = false;
-	}
-	else if (code == SC_LNPH)
-	{
-		isLinePhonoSWChanged = false;
-	}
-	else if (code >= SC_LCONTROL && code <= SC_RGUI)
-	{
-		keyboardHID.modifiers &= ~(1 << (code - SC_LCONTROL));
-	}
-	else
-	{
-		for (int k = 0; k < 6; k++)
-		{
-			if (keyboardHID.key[k] == code)
-			{
-				keyboardHID.key[k] = 0;
-			}
-		}
-	}
+    if (code == SC_LAYOUT)
+    {
+        isKeymapIDChanged = false;
+    }
+    else if (code == SC_LNPH)
+    {
+        isLinePhonoSWChanged = false;
+    }
+    else if (code >= SC_LCONTROL && code <= SC_RGUI)
+    {
+        keyboardHID.modifiers &= ~(1 << (code - SC_LCONTROL));
+    }
+    else
+    {
+        for (int k = 0; k < 6; k++)
+        {
+            if (keyboardHID.key[k] == code)
+            {
+                keyboardHID.key[k] = 0;
+            }
+        }
+    }
 
-	longPressCounter = 0;
+    longPressCounter = 0;
 }
 
 void setKeys(uint8_t code)
 {
-	if (code == SC_LAYOUT)
-	{
-		if (!isKeymapIDChanged)
-		{
-			if (keymapID == 0)
-			{
-				keymapID = 1;
-				HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
-			}
-			else
-			{
-				keymapID = 0;
-				HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
-			}
-			isKeymapIDChanged = true;
-		}
-	}
-	else if (code == SC_LNPH)
-	{
-		if (!isLinePhonoSWChanged)
-		{
-			setLinePhonoSW(!linePhonoSW);
-			setBootDfuFlag(false);
-			isLinePhonoSWChanged = true;
-		}
-	}
-	else if (code >= SC_LCONTROL && code <= SC_RGUI)
-	{
-		keyboardHID.modifiers |= 1 << (code - SC_LCONTROL);
-	}
-	else
-	{
-		for (int k = 0; k < 6; k++)
-		{
-			if (keyboardHID.key[k] == code)
-			{
-				// LGUI + ESC 長押しでDFUモーでリセット
-				if (keyboardHID.modifiers == 0x08 && keyboardHID.key[k] == SC_ESC)
-				{
-					longPressCounter++;
-					if (longPressCounter == 5000)
-					{
-						setBootDfuFlag(true);
-						SEGGER_RTT_printf(0, "Boot Custom DFU...\n");
-						HAL_Delay(100);
-						NVIC_SystemReset();
-					}
-				}
-				break;
-			}
-			else if (keyboardHID.key[k] == 0x00)
-			{
-				keyboardHID.key[k] = code;
-				break;
-			}
-		}
-	}
+    if (code == SC_LAYOUT)
+    {
+        if (!isKeymapIDChanged)
+        {
+            if (keymapID == 0)
+            {
+                keymapID = 1;
+                HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
+            }
+            else
+            {
+                keymapID = 0;
+                HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
+            }
+            isKeymapIDChanged = true;
+        }
+    }
+    else if (code == SC_LNPH)
+    {
+        if (!isLinePhonoSWChanged)
+        {
+            setLinePhonoSW(!linePhonoSW);
+            setBootDfuFlag(false);
+            isLinePhonoSWChanged = true;
+        }
+    }
+    else if (code >= SC_LCONTROL && code <= SC_RGUI)
+    {
+        keyboardHID.modifiers |= 1 << (code - SC_LCONTROL);
+    }
+    else
+    {
+        for (int k = 0; k < 6; k++)
+        {
+            if (keyboardHID.key[k] == code)
+            {
+                // LGUI + ESC 長押しでDFUモーでリセット
+                if (keyboardHID.modifiers == 0x08 && keyboardHID.key[k] == SC_ESC)
+                {
+                    longPressCounter++;
+                    if (longPressCounter == 5000)
+                    {
+                        setBootDfuFlag(true);
+                        SEGGER_RTT_printf(0, "Boot Custom DFU...\n");
+                        HAL_Delay(100);
+                        NVIC_SystemReset();
+                    }
+                }
+                break;
+            }
+            else if (keyboardHID.key[k] == 0x00)
+            {
+                keyboardHID.key[k] = code;
+                break;
+            }
+        }
+    }
 }
 
 void hid_keyscan_task(void)
 {
-	static int i = 0;
+    static int i = 0;
 
-	switch (i)
-	{
-	case 0:
-		HAL_GPIO_WritePin(HC164_A_GPIO_Port, HC164_A_Pin, GPIO_PIN_RESET);
-	case 1:
-	case 2:
-	case 3:
-	case 4:
-		HAL_GPIO_WritePin(HC164_CLK_GPIO_Port, HC164_CLK_Pin, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(HC164_CLK_GPIO_Port, HC164_CLK_Pin, GPIO_PIN_RESET);
+    switch (i)
+    {
+    case 0:
+        HAL_GPIO_WritePin(HC164_A_GPIO_Port, HC164_A_Pin, GPIO_PIN_RESET);
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        HAL_GPIO_WritePin(HC164_CLK_GPIO_Port, HC164_CLK_Pin, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(HC164_CLK_GPIO_Port, HC164_CLK_Pin, GPIO_PIN_RESET);
 
-		HAL_GPIO_WritePin(HC164_A_GPIO_Port, HC164_A_Pin, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(HC164_A_GPIO_Port, HC164_A_Pin, GPIO_PIN_SET);
 
-		HAL_GPIO_WritePin(HC165_SL_GPIO_Port, HC165_SL_Pin, GPIO_PIN_RESET);
-		HAL_Delay(1);
-		HAL_GPIO_WritePin(HC165_SL_GPIO_Port, HC165_SL_Pin, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(HC165_SL_GPIO_Port, HC165_SL_Pin, GPIO_PIN_RESET);
+        HAL_Delay(1);
+        HAL_GPIO_WritePin(HC165_SL_GPIO_Port, HC165_SL_Pin, GPIO_PIN_SET);
 
-		for (int j = 0; j < 16; j++)
-		{
-			uint8_t jj = 255;
-			if (j < 8)
-			{
-				jj = j + 5;
-			}
-			else if (j >= 11 && j < 16)
-			{
-				jj = j - 11;
-			}
+        for (int j = 0; j < 16; j++)
+        {
+            uint8_t jj = 255;
+            if (j < 8)
+            {
+                jj = j + 5;
+            }
+            else if (j >= 11 && j < 16)
+            {
+                jj = j - 11;
+            }
 
-			if (jj < MATRIX_COLUMNS)
-			{
-				if (HAL_GPIO_ReadPin(HC165_QH_GPIO_Port, HC165_QH_Pin))
-				{
-					keyState[i] &= ~((uint16_t)1 << jj);
+            if (jj < MATRIX_COLUMNS)
+            {
+                if (HAL_GPIO_ReadPin(HC165_QH_GPIO_Port, HC165_QH_Pin))
+                {
+                    keyState[i] &= ~((uint16_t) 1 << jj);
 
-					if (keyState[i] != prevKeyState[i])
-					{
-						uint8_t keycode = getKeyCode(keymapID, i, (MATRIX_COLUMNS - 1) - jj);
-						clearKeys(keycode);
-					}
-				}
-				else
-				{
-					keyState[i] |= ((uint16_t)1 << jj);
+                    if (keyState[i] != prevKeyState[i])
+                    {
+                        uint8_t keycode = getKeyCode(keymapID, i, (MATRIX_COLUMNS - 1) - jj);
+                        clearKeys(keycode);
+                    }
+                }
+                else
+                {
+                    keyState[i] |= ((uint16_t) 1 << jj);
 
-					uint8_t keycode = getKeyCode(keymapID, i, (MATRIX_COLUMNS - 1) - jj);
-					setKeys(keycode);
-				}
-			}
+                    uint8_t keycode = getKeyCode(keymapID, i, (MATRIX_COLUMNS - 1) - jj);
+                    setKeys(keycode);
+                }
+            }
 
-			HAL_GPIO_WritePin(HC165_CLK_GPIO_Port, HC165_CLK_Pin, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(HC165_CLK_GPIO_Port, HC165_CLK_Pin, GPIO_PIN_RESET);
-		}
+            HAL_GPIO_WritePin(HC165_CLK_GPIO_Port, HC165_CLK_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(HC165_CLK_GPIO_Port, HC165_CLK_Pin, GPIO_PIN_RESET);
+        }
 
-		i++;
-		break;
-	case 5:
-		for (int i = 0; i < MATRIX_ROWS; i++)
-		{
-			if (keyState[i] != 0x0 || (keyState[i] == 0x0 && keyState[i] != prevKeyState[i]))
-			{
-				if (!tud_hid_ready())
-					return;
+        i++;
+        break;
+    case 5:
+        for (int i = 0; i < MATRIX_ROWS; i++)
+        {
+            if (keyState[i] != 0x0 || (keyState[i] == 0x0 && keyState[i] != prevKeyState[i]))
+            {
+                if (!tud_hid_ready())
+                    return;
 
-				tud_hid_keyboard_report(REPORT_ID_KEYBOARD, keyboardHID.modifiers, keyboardHID.key);
-				break;
-			}
-		}
+                tud_hid_keyboard_report(REPORT_ID_KEYBOARD, keyboardHID.modifiers, keyboardHID.key);
+                break;
+            }
+        }
 
-		for (int i = 0; i < MATRIX_ROWS; i++)
-		{
-			prevKeyState[i] = keyState[i];
-		}
+        for (int i = 0; i < MATRIX_ROWS; i++)
+        {
+            prevKeyState[i] = keyState[i];
+        }
 
-		i = 0;
-		break;
-	default:
-		i = 0;
-		break;
-	}
+        i = 0;
+        break;
+    default:
+        i = 0;
+        break;
+    }
 }
