f7c2f18e9ea0e15f23a2988b7d0561c590ff5e8c
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2024-09-02 16:05:10 +0900

    Modified USB max current from 100mA to 500mA.

--- STM32CubeIDE/C4NDY_STK/Core/Src/usb_descriptors.c
+++ STM32CubeIDE/C4NDY_STK/Core/Src/usb_descriptors.c
@@ -126,7 +126,7 @@ uint8_t const * tud_hid_descriptor_report_cb(uint8_t itf)
 uint8_t const desc_configuration[] =
 {
   // Config number, interface count, string index, total length, attribute, power in mA
-  TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
+  TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 500),
 
   // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
   TUD_HID_DESCRIPTOR(ITF_NUM_HID,
