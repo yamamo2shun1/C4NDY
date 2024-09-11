a40cc64f29ad62023f3cd1153acb38dc7c5bf1d3
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2024-09-11 21:16:27 +0900

    Fixed Google Analytics.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-export default {extends: DefaultTheme,
-                head:
-                    [
-                        [ "script", {async : "", src : "https://www.googletagmanager.com/gtag/js?id=G-EHFBVRHX4Q"} ],
-                        [ "script", {}, `window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-EHFBVRHX4');` ]
-                    ]
-}
+export default DefaultTheme
